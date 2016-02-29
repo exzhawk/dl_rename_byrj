6e46c46e2a3ea4bcd0ce649c16692edde285e8e4
Epix <git@exz.me>
2016-02-29 17:11:58 +0800

    replace output format to pickle

--- offline2db.py
+++ offline2db.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 # Author: Epix
 import codecs
-import json
 import os
+import pickle
 import re
 
 from lxml import etree
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     rj_re = re.compile(r'/(RJ\d*.)\.html')
     offline_dir = 'offline'
     files = os.listdir(offline_dir)
-    result = []
+    result_list = []
+    result = {}
     for f in files:
         html_file = codecs.open(os.path.join(offline_dir, f), 'r', 'utf8')
         tree = etree.HTML(html_file.read())
@@ -23,9 +24,13 @@ if __name__ == '__main__':
                 url = unicode(title_element.xpath('./@href')[0])
                 group = unicode(post.xpath('./dd[@class="maker_name"]/a/text()')[0])
                 rj_number = rj_re.search(url).group(1)
-                result.append({'rj': rj_number, 'title': title, 'group': group})
+                result_list.append({'rj': rj_number, 'title': title, 'group': group})
+                result[rj_number] = [title, group]
             except:
-                print(url)
-    with codecs.open('rj.jl', 'w', 'utf8') as f_out:
-        for p in result:
-            f_out.write(json.dumps(p, ensure_ascii=False) + '\n')
+                print(post.xpath('./dt[@class="work_name"]/a/@href')[0])
+
+    pickle.dump(result, open('db.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
+    # with codecs.open('rj.jl', 'w', 'utf8') as f_out:
+    #     for p in result:
+    #         f_out.write(json.dumps(p, ensure_ascii=False) + '\n')
